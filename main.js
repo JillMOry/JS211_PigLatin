67a7522b8fed5a776a5ba3908743dc5691aeb15c
JillMOry <oryjill@gmail.com>
2020-02-09 17:17:23 -0600

    recommitting to correct branch

--- main.js
+++ main.js
@@ -117,3 +117,5 @@ const reload = () => {
 // 1. if word begins with a vowel send to one function: adds "yay"
 // 2. if word begins in with a consonant send to another function: splices off beginning, returns word with new ending.
 // 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
+
+//
